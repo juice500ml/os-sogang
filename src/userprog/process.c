39fa37bd3179a26f615fd463ac85135fdd9e6a4a
juice500ml <juice500ml@gmail.com>
2015-10-15 18:07:49 +0900

    quick fix

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -398,9 +398,8 @@ validate_segment (const struct Elf32_Phdr *phdr, struct file *file)
      it then user code that passed a null pointer to system calls
      could quite likely panic the kernel by way of null pointer
      assertions in memcpy(), etc. */
-  if (phdr->p_offset < PGSIZE)
+  if (phdr->p_vaddr < PGSIZE)
     return false;
-  // TODO: Dies here!
 
   /* It's okay. */
   return true;
