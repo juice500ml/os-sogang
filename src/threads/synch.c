284f20946de65da73104c9c046d578972fd5d37b
juice500ml <juice500ml@gmail.com>
2015-11-28 00:24:51 +0900

    minor fixes

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -68,8 +68,8 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      // Priority insertion for semaphore
-      list_insert_ordered (&sema->waiters, &thread_current ()->elem, list_thread_priority_less, NULL);
+      // insertion for semaphore
+      list_push_back (&sema->waiters, &thread_current ()->elem);
       thread_block ();
     }
   sema->value--;
@@ -115,15 +115,14 @@ sema_up (struct semaphore *sema)
 
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters)) {
-      // priority may change
+      // priority may have changed
       list_sort(&sema->waiters, list_thread_priority_less, NULL);
       thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                   struct thread, elem));
   }
   sema->value++;
   intr_set_level (old_level);
-  // unblock then order
-  thread_yield();
+  thread_yield ();
 }
 
 static void sema_test_helper (void *sema_);
