a26d247130e7cec4be82cac5023b9ff0f1729ba7
juice500ml <juice500ml@gmail.com>
2015-12-22 16:37:51 +0900

    page struct and list created.

--- src/vm/page.c
+++ src/vm/page.c
@@ -1,6 +1,10 @@
 #include "vm/page.h"
 #include "userprog/pagedir.h"
 #include "threads/thread.h"
+#include "threads/malloc.h"
+
+// List of all pages
+static struct list all_pages = LIST_INITIALIZER (all_pages);
 
 /* Adds a mapping from user virtual address UPAGE to kernel
    virtual address KPAGE to the page table.
@@ -11,7 +15,7 @@
    with palloc_get_page().
    Returns true on success, false if UPAGE is already mapped or
    if memory allocation fails.
-   Moved to here from process.h */
+   Moved to here from userprog/process.h. */
 bool
 install_page (void *upage, void *kpage, bool writable)
 {
@@ -19,6 +23,32 @@ install_page (void *upage, void *kpage, bool writable)
 
   /* Verify that there's not already a page at that virtual
      address, then map our page there. */
-  return (pagedir_get_page (th->pagedir, upage) == NULL
-          && pagedir_set_page (th->pagedir, upage, kpage, writable));
+  if(pagedir_get_page (th->pagedir, upage) == NULL
+     && pagedir_set_page (th->pagedir, upage, kpage, writable)) {
+      // Traverse through every pages to find kpage
+      // which is used by current thread.
+      struct list_elem *e;
+      for(e=list_begin(&all_pages); e!=list_end(&all_pages); e=list_next(e)) {
+          struct page *p = list_entry(e, struct page, elem);
+          // If found, map kpage and upage
+          if(p->kpage==kpage && p->th == th) {
+              ASSERT (p->upage == NULL);
+              p->upage = upage;
+              return true;
+          }
+      }
+  }
+  return false;
 }
+
+// Add a kpage which is used by current thread.
+void
+add_page (void *kpage)
+{
+  struct page *p = malloc(sizeof(struct page));
+  p->upage = NULL;
+  p->kpage = kpage;
+  p->th = thread_current ();
+  list_push_back (&all_pages, &p->elem);
+}
+
--- src/vm/page.h
+++ src/vm/page.h
@@ -4,7 +4,17 @@
 #include <debug.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <list.h>
+
+struct page
+  {
+    void *upage;
+    void *kpage;
+    struct thread *th;
+    struct list_elem elem;
+  };
 
 bool install_page (void *upage, void *kpage, bool writable);
+void add_page (void *kpage);
 
 #endif /* vm/page.h */
