fc1726d8c64af739b056fe65388cc00e44364d46
juice500ml <juice500ml@gmail.com>
2015-12-22 23:19:17 +0900

    frame.c frame.h added. missed adding before.

---
+++ src/vm/frame.c
@@ -0,0 +1,14 @@
+#include "vm/frame.h"
+#include "vm/page.h"
+#include "userprog/pagedir.h"
+#include "threads/palloc.h"
+
+// wrapper for palloc
+// get user frame
+void *get_frame(enum palloc_flags flags)
+{
+  void *kpage = palloc_get_page (flags);
+  if (flags == PAL_USER || flags == (PAL_USER | PAL_ZERO))
+    add_page (kpage);
+  return kpage;
+}
---
+++ src/vm/frame.h
@@ -0,0 +1,12 @@
+#ifndef VM_FRAME_H
+#define VM_FRAME_H
+
+#include <debug.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <list.h>
+#include "threads/palloc.h"
+
+void *get_frame(enum palloc_flags);
+
+#endif /* vm/frame.h */
